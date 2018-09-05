3ecc0972ca0364ab1b7517bcb00a460586f943e6
aniket sharma <aniket965.as@gmail.com>
2018-09-05 21:02:22 +0530

    Loaded Iframe Api

--- renderer.js
+++ renderer.js
@@ -3,19 +3,60 @@
 // All of the Node.js APIs are available in this process.
 
 let {clipboard, ipcRenderer} = require('electron')
+let player;
+
+// promise for loading Youtube Api
+let apiPromise  = new Promise(resolve => {
+  window.onYouTubeIframeAPIReady = () => {
+//     console.log("YouTube API loaded");
+    resolve();
+  }
+  const tag = document.createElement('script');
+  tag.src = "https://www.youtube.com/iframe_api";
+  const firstScriptTag = document.getElementsByTagName('script')[0];
+  firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+});
 
 
-function pasted(){
+async function pasted() {
 	let url = clipboard.readText('selection')
-	if(web = generateYoutubeUrl(url)){
-	  document.querySelector("#video").innerHTML = web
-	  // alert(document.querySelector("#video").innerHTML)
-	}
+	if(web = generateYoutubeUrl(url)) {
+    await apiPromise
+    player = new YT.Player(document.querySelector("#video"), {
+      height: '100%',
+      width: '100%',
+      videoId: web,
+      playerVars: { 'autoplay': 1},
+      events: {
+        onReady: (event) => {
+        // console.log(event)
+        },
+        onStateChange: (event) => {
+          // console.log(event)
+        }
+      }
+    });
+
+   }
 	else
 	  alert("Invalid Url, we only support youtube for now");
 }
 
-
+function pause() {
+ if(player) {
+   player.pauseVideo()
+ }else {
+  alert("Play a video first");
+ }
+}
+ 
+function play() {
+  if(player) {
+    player.playVideo()
+  }else {
+   alert("Play a video first");
+  }
+ }
 function generateYoutubeUrl(url) {    
   if (url != undefined || url != '') {        
       var regExp = /^.*(youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=|\&v=|\?v=)([^#\&\?]*).*/;
@@ -23,8 +64,8 @@ function generateYoutubeUrl(url) {
       if (match && match[2].length == 11) {
           // Do anything for being valid
           // if need to change the url to embed url then use below line            
-          let web =  'https://www.youtube.com/embed/' + match[2] + '?autoplay=1&enablejsapi=1'
-          return `<iframe style="position:fixed; top:0px; left:0px; bottom:0px; right:0px; width:100%; height:100%; border:none; margin:0; padding:0; overflow:hidden; z-index:999999;" src="${web}"></iframe>`
+          // let web =  'https://www.youtube.com/embed/' + match[2] + '?autoplay=1&version=3&enablejsapi=1'
+          return match[2]
       } else {
         return false;
       }
@@ -36,6 +77,13 @@ ipcRenderer.on('newlink', (ev, arg) => {
 	pasted()
 })
 
+ipcRenderer.on('pause', (ev, arg) => {
+	pause()
+})
+
+ipcRenderer.on('play', (ev, arg) => {
+	play()
+})
 
 ipcRenderer.on('newframe', (ev, arg) => {
 	document.querySelector('#video').innerHTML = arg
