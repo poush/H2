42d59688be99848ea7d46c94f631e5ec87f3aa19
Maham Shahid <mahum.shahid934@gmail.com>
2018-10-13 20:12:36 +0500

    Issue-24: Added complete landing page

---
+++ gulpfile.js
@@ -0,0 +1,29 @@
+'use strict';
+
+const gulp = require('gulp');
+const gutil = require('gulp-util');
+const sass = require('gulp-sass');
+const rename = require('gulp-rename');
+const cssmin = require('gulp-cssmin');
+
+gulp.task('default', () => {
+    gutil.log('Gulp is running!');
+    gulp.watch('styles/sass/landing.scss', gulp.series('sass-to-css', 'minify-css'));
+});
+
+gulp.task('sass-to-css', () => {
+    return gulp.src('./styles/sass/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('./styles/css'));
+});
+
+gulp.task('minify-css', () => {
+    return gulp.src('./styles/css/landing.css')
+        .pipe(cssmin().on('error', function (err) {
+            gutil.log(gutil.colors.red('[Error]'), err.toString());
+        }))
+        .pipe(rename({
+            suffix: '.min'
+        }))
+        .pipe(gulp.dest('./styles/css'));
+});
