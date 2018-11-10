8aa0c4d0ab9772edadd464734433fc9b45ce09ca
Piyush Agrawal <poush12@gmail.com>
2018-11-11 02:39:41 +0530

    added: ActionManager (#89)

---
+++ src/config/actions.js
@@ -0,0 +1,65 @@
+const {app} = require("electron");
+
+const providers = require("../ServiceProviders/providers");
+const fullscreenToggle = require("../lib/fullscreen-toggle");
+
+
+module.exports = {
+  keys: [
+    {
+      key: "CommandOrControl+Shift+V",
+      action: (mainWin) => {
+        return () => {
+          providers.run(mainWin);
+        }
+      }
+    },
+    {
+      key: "CommandOrControl+H+Q",
+      action: (mainWin) => {
+        return () => {
+          app.quit()
+        }
+      }
+    },
+
+    {
+      key: "CommandOrControl+Shift+1",
+      action: (mainWin) => {
+        return () => {
+          mainWin.webContents.send("pause", "ping");
+        }
+      }
+    },
+    {
+      key: "CommandOrControl+Shift+2",
+      action: (mainWin) => {
+        return () => {
+          mainWin.webContents.send("play", "ping");
+        }
+      }
+    },
+    {
+      key: "Alt+Shift+F",
+      action: (mainWin) => {
+        return () => {
+          fullscreenToggle(mainWin, false);
+        }
+      }
+    },
+    {
+      key: "CommandOrControl+Shift+T",
+      action: (mainWin) => {
+        // toggles translucent mode
+        return () => {
+          let translucency = 1;
+          if (mainWin.getOpacity() == 1) {
+            translucency = 0.7;
+          }
+          mainWin.setOpacity(translucency);
+          mainWin.setIgnoreMouseEvents(translucency < 1);
+        }
+      }
+    }
+  ]
+}
