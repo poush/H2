d0adfe24c0602390d3d688fbb94124aff8c1af20
Piyush Agrawal <poush12@gmail.com>
2018-11-10 17:41:03 +0530

    project structure updated

--- src/ServiceProviders/mediaProviderApplier.js
+++ src/ServiceProviders/mediaProviderApplier.js
@@ -1,9 +1,10 @@
 const baseMediaProvider = require('./MediaProviders/baseMediaProvider')
+const config = require('../lib/config')
 
 module.exports = function (toApply, win) {
   if (toApply instanceof baseMediaProvider) {
     if (toApply.response.type === 'iframe') {
-      win.loadFile('./index.html')
+      win.loadFile(config.indexPath)
 
       win.webContents.once('dom-ready', () => {
         win.webContents.send(
---
+++ src/lib/config.js
@@ -0,0 +1,8 @@
+const path = require('path')
+
+module.exports = {
+
+  indexPath: path.join(__dirname, '..', 'index.html'),
+
+  trayIconPath: path.join(__dirname, '..', 'assets', 'images', 'tray.png')
+}
