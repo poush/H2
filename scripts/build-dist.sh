f5fca0a4e5b162f57b840ca29070d95758bea3d3
Piyush Agrawal <poush12@gmail.com>
2018-11-11 22:50:23 +0530

    travis fixes

---
+++ scripts/build-dist.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
+
+    # Install some custom requirements on OS X
+    # e.g. brew install pyenv-virtualenv
+    npm run build
+else
+    # Install some custom requirements on Linux
+fi
